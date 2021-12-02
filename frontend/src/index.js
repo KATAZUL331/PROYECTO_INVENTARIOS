6fc7ad46d4c79ae3e16486c2c4942939fe02e923
KAT AZUL <katazul331@gmail.com>
2021-12-01 21:24:53 -0500

    01/12/2021

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,28 +8,14 @@ import Axios from 'axios'
 
 Axios.defaults.baseURL = 'http://localhost:4000'
 
-*
-ReactDOM.render( < React.StrictMode >
-
-    <
-    App / >
-
-    <
-    /React.StrictMode> ,
-
-    document.getElementById('root')
-);
-
-reportWebVitals();
-
-/*
-ReactDOM.render( 
+ReactDOM.render(
     <React.StrictMode>
-
-        <App/>
-
-    </React.StrictMode> ,
-
+        <App />
+    </React.StrictMode>,
     document.getElementById('root')
 );
-*/
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
