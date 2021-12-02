a0fa715c2413b6d7e46ff641b6d031f8f44e4d15
KAT AZUL <katazul331@gmail.com>
2021-12-01 21:14:27 -0500

    01/12/2021

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,20 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Axios from 'axios'
 
 Axios.defaults.baseURL = 'http://localhost:4000'
 
-ReactDOM.render( 
-    <React.StrictMode>
+*
+ReactDOM.render( < React.StrictMode >
 
-        <App/>
+    <
+    App / >
 
-    </React.StrictMode> ,
+    <
+    /React.StrictMode> ,
 
     document.getElementById('root')
 );
+
+reportWebVitals();
 
 /*
 ReactDOM.render( 
